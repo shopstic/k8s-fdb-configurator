f5eaa59f94e7f40975cf1a0d404001a55b293381
Jacky Nguyen <nktpro@gmail.com>
2021-04-12 17:43:21 -0400

    Separate label and annotation to workaround K8s label value limit (only 63 chars)

--- src/actions/prepare-local-pv.ts
+++ src/actions/prepare-local-pv.ts
@@ -15,13 +15,15 @@ const logger = loggerWithContext("main");
 export default createCliAction(
   Type.Object({
     nodeNameEnvVarName: NonEmptyString(),
-    pendingDeviceIdsLabelName: NonEmptyString(),
+    labelName: NonEmptyString(),
+    pendingDeviceIdsAnnotationName: NonEmptyString(),
     rootMountPath: NonEmptyString(),
   }),
   async (
     {
       nodeNameEnvVarName,
-      pendingDeviceIdsLabelName,
+      labelName,
+      pendingDeviceIdsAnnotationName,
       rootMountPath,
     },
   ) => {
@@ -31,123 +33,122 @@ export default createCliAction(
       throw new Error(`${nodeNameEnvVarName} env variable is not set`);
     }
 
-    const nodeLabels = await kubectlGetJson({
+    const nodeAnnotations = await kubectlGetJson({
       args: [
         `node/${nodeName}`,
-        "-o=jsonpath={.metadata.labels}",
+        "-o=jsonpath={.metadata.annotations}",
       ],
       schema: Type.Dict(Type.String()),
     });
 
     const deviceIdsString =
-      (typeof nodeLabels[pendingDeviceIdsLabelName] === "string")
-        ? nodeLabels[pendingDeviceIdsLabelName]
+      (typeof nodeAnnotations[pendingDeviceIdsAnnotationName] === "string")
+        ? nodeAnnotations[pendingDeviceIdsAnnotationName]
         : "";
     const deviceIds = deviceIdsString.split(",");
 
     if (deviceIds.length === 0) {
       logger.info(
-        `Node label '${pendingDeviceIdsLabelName}' is empty, nothing to do`,
+        `Node annotation '${pendingDeviceIdsAnnotationName}' is empty, nothing to do`,
       );
-      return ExitCode.Zero;
-    }
-
-    logger.info(
-      `Going to prepare the following ${deviceIds.length} devices: ${
-        deviceIds.join(", ")
-      }`,
-    );
-
-    for (const deviceId of deviceIds) {
-      const devicePath = joinPath(
-        "/dev/disk/by-id",
-        deviceId,
+    } else {
+      logger.info(
+        `Going to prepare the following ${deviceIds.length} devices: ${
+          deviceIds.join(", ")
+        }`,
       );
-      const mountPath = joinPath(rootMountPath, deviceId);
-      const mountpointCheck = Deno.run({
-        cmd: toRootElevatedCommand(["mountpoint", mountPath]),
-        stdout: "null",
-        stderr: "null",
-      });
-      const isMounted = (await mountpointCheck.status()).code === 0;
 
-      if (!isMounted) {
-        logger.info(`${mountPath} is not mounted`);
-        logger.info(`Checking for existing file system inside ${devicePath}`);
-
-        const wipefsTest = await captureExec({
-          run: {
-            cmd: toRootElevatedCommand(["wipefs", "-a", "-n", devicePath]),
-          },
-        });
-
-        if (wipefsTest.trim().length > 0) {
-          logger.error(
-            `Device possibly contains an existing file system, wipefs test output: ${wipefsTest}`,
-          );
-          return ExitCode.One;
-        }
-
-        logger.info(
-          `Making sure /etc/fstab does not already contain a reference to ${devicePath}`,
+      for (const deviceId of deviceIds) {
+        const devicePath = joinPath(
+          "/dev/disk/by-id",
+          deviceId,
         );
-        const currentFstabContent = await captureExec({
-          run: { cmd: toRootElevatedCommand(["cat", "/etc/fstab"]) },
+        const mountPath = joinPath(rootMountPath, deviceId);
+        const mountpointCheck = Deno.run({
+          cmd: toRootElevatedCommand(["mountpoint", mountPath]),
+          stdout: "null",
+          stderr: "null",
         });
+        const isMounted = (await mountpointCheck.status()).code === 0;
 
-        if (currentFstabContent.indexOf(devicePath) !== -1) {
-          logger.error(
-            `Device ${devicePath} found inside /etc/fstab`,
-          );
-          return ExitCode.One;
-        }
+        if (!isMounted) {
+          logger.info(`${mountPath} is not mounted`);
+          logger.info(`Checking for existing file system inside ${devicePath}`);
 
-        logger.info(`Formatting ${devicePath}`);
-        await inheritExec({
-          run: { cmd: toRootElevatedCommand(["mkfs.ext4", devicePath]) },
-        });
-
-        logger.info(`Writing ${devicePath} to /etc/fstab`);
-        await inheritExec({
-          run: { cmd: toRootElevatedCommand(["tee", "/etc/fstab"]) },
-          stdin: currentFstabContent + "\n" +
-            `${devicePath}  ${mountPath}  ext4  defaults,noatime,discard,nofail  0 0`,
-        });
-
-        logger.info(`Creating mount path ${mountPath}`);
-        await inheritExec({
-          run: {
-            cmd: toRootElevatedCommand(["mkdir", "-p", mountPath]),
-          },
-        });
-
-        logger.info(`Making mount path ${mountPath} immutable`);
-        await inheritExec({
-          run: { cmd: toRootElevatedCommand(["chattr", "+i", mountPath]) },
-        });
-
-        logger.info(`Mounting ${mountPath}`);
-        await inheritExec(
-          {
+          const wipefsTest = await captureExec({
             run: {
-              cmd: toRootElevatedCommand(["mount", `--source=${devicePath}`]),
+              cmd: toRootElevatedCommand(["wipefs", "-a", "-n", devicePath]),
             },
-          },
-        );
-      } else {
-        logger.info(`${mountPath} is already a mountpoint, nothing to do`);
+          });
+
+          if (wipefsTest.trim().length > 0) {
+            logger.error(
+              `Device possibly contains an existing file system, wipefs test output: ${wipefsTest}`,
+            );
+            return ExitCode.One;
+          }
+
+          logger.info(
+            `Making sure /etc/fstab does not already contain a reference to ${devicePath}`,
+          );
+          const currentFstabContent = await captureExec({
+            run: { cmd: toRootElevatedCommand(["cat", "/etc/fstab"]) },
+          });
+
+          if (currentFstabContent.indexOf(devicePath) !== -1) {
+            logger.error(
+              `Device ${devicePath} found inside /etc/fstab`,
+            );
+            return ExitCode.One;
+          }
+
+          logger.info(`Formatting ${devicePath}`);
+          await inheritExec({
+            run: { cmd: toRootElevatedCommand(["mkfs.ext4", devicePath]) },
+          });
+
+          logger.info(`Writing ${devicePath} to /etc/fstab`);
+          await inheritExec({
+            run: { cmd: toRootElevatedCommand(["tee", "/etc/fstab"]) },
+            stdin: currentFstabContent + "\n" +
+              `${devicePath}  ${mountPath}  ext4  defaults,noatime,discard,nofail  0 0`,
+          });
+
+          logger.info(`Creating mount path ${mountPath}`);
+          await inheritExec({
+            run: {
+              cmd: toRootElevatedCommand(["mkdir", "-p", mountPath]),
+            },
+          });
+
+          logger.info(`Making mount path ${mountPath} immutable`);
+          await inheritExec({
+            run: { cmd: toRootElevatedCommand(["chattr", "+i", mountPath]) },
+          });
+
+          logger.info(`Mounting ${mountPath}`);
+          await inheritExec(
+            {
+              run: {
+                cmd: toRootElevatedCommand(["mount", `--source=${devicePath}`]),
+              },
+            },
+          );
+        } else {
+          logger.info(`${mountPath} is already a mountpoint, nothing to do`);
+        }
       }
     }
 
     logger.info(
-      `Removing '${pendingDeviceIdsLabelName}' label from node ${nodeName}`,
+      `Removing '${labelName}' label from node ${nodeName}`,
     );
 
     await kubectlInherit({
       args: [
         "label",
         `node/${nodeName}`,
-        `${pendingDeviceIdsLabelName}-`,
+        `${labelName}-`,
       ],
     });
 
